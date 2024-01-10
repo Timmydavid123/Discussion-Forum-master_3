f1461eeac0040ba49642efe69cad0b30b4969847
Timmydavid123 <114180127+Timmydavid123@users.noreply.github.com>
2024-01-10 12:25:32 +0100

    wjenmf

--- Discussion-Forum-master/src/components/common/jumbotron.jsx
+++ Discussion-Forum-master/src/components/common/jumbotron.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import './jumbotron.css'
 
 const Jumbotron = () => {
   return (
     <div className="jumbotron">
       <div className="search-container">
-        <img src="smooth-icon.png" alt="Smooth Icon" className="smooth-icon" />
-        <input type="text" placeholder="Search..." className="search-box" />
+        <img src="logo1.png" alt="images" className="smooth-icon" />
+        <input
+          type="text"
+          placeholder="Ask me anything"
+          className="search-box"
+          style={{ width: '600px' }}
+        />
+        <button className="search-button">Search</button>
       </div>
-
     </div>
   );
 };
--- Discussion-Forum-master/src/components/navbar.jsx
+++ Discussion-Forum-master/src/components/navbar.jsx
@@ -3,38 +3,38 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = ({ user }) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark  sticky-top ">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
       <NavLink className="navbar-brand" to="/">
         Xooth
       </NavLink>
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         type="button"
         data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false" 
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse row" id="navbarColor03">
+      <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <NavLink className="nav-link ml-3" to="/">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/">
               Home
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link ml-2" to="/dashboard">
+            <NavLink className="nav-link" to="/dashboard">
               Forum
             </NavLink>
           </li>
         </ul>
-        <ul className="nav navbar-nav navbar-right">
+        <ul className="navbar-nav ml-auto">
           {!user && (
-            <React.Fragment>
+            <>
               <li className="nav-item">
                 <NavLink className="nav-link" to="/users/login">
                   Login
@@ -45,10 +45,10 @@ const NavBar = ({ user }) => {
                   Register
                 </NavLink>
               </li>
-            </React.Fragment>
+            </>
           )}
           {user && (
-            <React.Fragment>
+            <>
               <li className="nav-item">
                 <NavLink className="nav-link" to="/me">
                   Hi {user.username}
@@ -59,7 +59,7 @@ const NavBar = ({ user }) => {
                   LogOut
                 </NavLink>
               </li>
-            </React.Fragment>
+            </>
           )}
         </ul>
       </div>
