86185b294d236daf87dcc56803b0efecf46a281b
Timmydavid123 <114180127+Timmydavid123@users.noreply.github.com>
2024-01-10 16:07:43 +0100

    vbdjk

--- Discussion-Forum-master/src/components/navbar.jsx
+++ Discussion-Forum-master/src/components/navbar.jsx
@@ -1,59 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const NavBar = ({ user }) => {
+  const [isCollapsed, setCollapsed] = useState(true);
+
+  const handleToggle = () => {
+    setCollapsed(!isCollapsed);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
       <NavLink className="navbar-brand" to="/">
-        Xooth
+        <span style={{ color: "white" }}>Xooth</span>
       </NavLink>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
 
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <NavLink className="nav-link" to="/dashboard">
+        <span style={{ color: "white" }}>Forum</span>
+      </NavLink>
+      
+      <NavLink className="nav-link" to="/news">
+        <span style={{ color: "white" }}>News</span>
+      </NavLink>
+
+      <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarNav">
+        {/* Items inside collapsible section */}
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
             <NavLink className="nav-link" to="/">
-              Home
+              <span style={{ color: "white" }}>Home</span>
             </NavLink>
           </li>
+
           <li className="nav-item">
-            <NavLink className="nav-link" to="/dashboard">
-              Forum
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/news">
-              News
-            </NavLink>
-          </li>
-          {/* Add the "Connect" link */}
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/connect">
-              Connect
-            </NavLink>
+          <NavLink className="nav-link" to="/connect">
+        <span style={{ color: "white" }}>Connect</span>
+      </NavLink>
           </li>
         </ul>
+
         <ul className="navbar-nav ml-auto">
           {!user && (
             <>
               <li className="nav-item">
                 <NavLink className="nav-link" to="/users/login">
-                  Login
+                  <span style={{ color: "white" }}>Login</span>
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink className="nav-link" to="/users/register">
-                  Register
+                  <span style={{ color: "white" }}>Register</span>
                 </NavLink>
               </li>
             </>
@@ -62,18 +57,31 @@ const NavBar = ({ user }) => {
             <>
               <li className="nav-item">
                 <NavLink className="nav-link" to="/me">
-                  Hi {user.username}
+                  <span style={{ color: "white" }}>Hi {user.username}</span>
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink className="nav-link" to="/users/logout">
-                  LogOut
+                  <span style={{ color: "white" }}>LogOut</span>
                 </NavLink>
               </li>
             </>
           )}
         </ul>
       </div>
+
+      <button
+        className={`navbar-toggler ${isCollapsed ? 'collapsed' : ''}`}
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded={!isCollapsed}
+        aria-label="Toggle navigation"
+        onClick={handleToggle}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
     </nav>
   );
 };
